47b8acc44724f1e25494644372a1d6b8051d236b
Ryoichida <zacharie.jammes@gmail.com>
2021-11-11 18:17:01 +0100

    Correct sed

--- setup.sh
+++ setup.sh
@@ -62,7 +62,7 @@ mkdir -p /mnt/var/cache/pacman
 mount /dev/vg00/pacman /mnt/var/cache/pacman
 mount /dev/vg00/tmp /mnt/tmp
 
-pacstrap /mnt base linux linux-firmware dhcpcd dhclient vim firefox sudo xorg-server xorg-xinit grub lvm2 archlinux-keyring virtualbox-guest-utils dmenu alacritty alsa-utils rsync reflector archlinux-wallpaper bash-completion lxappearance neofetch htop xdg-user-dirs picom networkmanager network-manager-applet zsh
+pacstrap /mnt base linux linux-firmware dhcpcd dhclient vim firefox sudo xorg-server xorg-xinit grub lvm2 virtualbox-guest-utils dmenu alacritty alsa-utils rsync reflector archlinux-wallpaper bash-completion lxappearance neofetch htop xdg-user-dirs picom networkmanager network-manager-applet zsh
 genfstab -U /mnt >> /mnt/etc/fstab
 
 arch-chroot /mnt /bin/bash <<EOF
@@ -77,11 +77,14 @@ echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 void.localdomain void" >> /etc/hosts
 echo root:root | chpasswd
+pacman -Syy archlinux-keyring --noconfirm
+pacman -Syy duf bpytop --noconfirm
 sed -i 's/fsck)/fsck systemd lvm2)/' /etc/mkinitcpio.conf
 mkinitcpio -P
 grub-install --target=i386-pc /dev/sda # replace sdx with your disk name, not the partition
 sed -i -e s/#GRUB_COLOR_/GRUB_COLOR_/g -e s/GRUB_GFXMODE=.*/GRUB_GFXMODE=1920x1080/ /etc/default/grub
 sed -i -e s@#GRUB_BACKGROUND=.*@GRUB_BACKGROUND=/usr/share/backgrounds/archlinux/simple.png@ /etc/default/grub 
+sed -i -e s@GRUB_COLOR_HIGHLIGHT="light-cyan/blue"@GRUB_COLOR_HIGHLIGHT="light-green/light-blue"@ /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg
 systemctl enable dhcpcd.service
 systemctl enable systemd-timesyncd.service
@@ -89,7 +92,7 @@ systemctl enable systemd-time-wait-sync.service
 systemctl enable vboxservice
 systemctl enable NetworkManager
 reflector -c France -a 6 --sort rate --save /etc/pacman.d/mirrorlist
-localectl set-x11-kbmap fr
+localectl set-x11-keymap fr
 useradd -m zac
 echo zac:root | chpasswd
 echo "zac ALL=(ALL) ALL" >> /etc/sudoers.d/zac
